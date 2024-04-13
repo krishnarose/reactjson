708a628b889fb3416be61bbd407b596174162cb2
krishnarose <krishnarose2001@gmail.com>
2024-04-13 13:55:20 +0530

    local json data get completed

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { data } from "./data";
 import { AiOutlineSearch } from "react-icons/ai";
 
 const Shop = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <>
       <div className="flex justify-between p-5  items-center mt-10 ">
@@ -16,6 +18,7 @@ const Shop = () => {
               type="text"
               className="  w-[300px] h-[40px] border border-black pl-3 font-semibold rounded-md shadow-xl"
               placeholder="Search products..."
+              onChange={(e) => setSearch(e.target.value)}
             />
           </div>
 
@@ -26,33 +29,54 @@ const Shop = () => {
       </div>
 
       <div className="flex justify-center items-center gap-10 w-full flex-wrap mt-10 ">
-        {data.map((apple) => {
-          return (
-            <>
-              <div
-                className="flex flex-col justify-start items-start gap-5"
-                key={apple.Id}
-              >
-                <img
-                  src={apple.image}
-                  alt={apple.title}
-                  className="w-[240px]"
-                />
-                <h2 className="font-semibold text-xl">{apple.title}</h2>
-                <h3 className="font-semibold">{apple.description}</h3>
-                <p className="text-xl font-semibold">
-                  {" "}
-                  <span className="text-xl font-exterbold"> ₹ </span>
-                  {apple.price}
-                </p>
-                <button className="bg-blue-400 text-xl font-semibold text-black rounded-xl px-3 py-2 hover:bg-blue-500">
-                  {" "}
-                  Buy Now
-                </button>
-              </div>
-            </>
-          );
-        })}
+        {data
+          .filter((iphone) => {
+            if (search === "") {
+              return true;
+            } else if (
+              iphone.title
+                .toLocaleLowerCase()
+                .includes(search.toLocaleLowerCase())
+            ) {
+              return true;
+            } else {
+              return false;
+            }
+          })
+          .map((apple) => {
+            return (
+              <>
+                <div
+                  className="flex flex-col justify-start items-start gap-5"
+                  key={apple.Id}
+                >
+                  <img
+                    src={apple.image}
+                    alt={apple.title}
+                    className="w-[240px]"
+                  />
+                  <h2 className="font-semibold text-xl">{apple.title}</h2>
+                  <h3 className="font-semibold">{apple.description}</h3>
+                  <p className="text-xl font-semibold">
+                    {" "}
+                    <span className="text-xl font-exterbold"> ₹ </span>
+                    {apple.price}
+                  </p>
+                  <button className="bg-blue-400 text-xl font-semibold text-black rounded-xl px-3 py-2 hover:bg-blue-500">
+                    {" "}
+                    Buy Now
+                  </button>
+                </div>
+              </>
+            );
+          })}
+        {data.filter((iphone) =>
+          iphone.title.toLocaleUpperCase().includes(search.toLocaleUpperCase())
+        ).length === 0 && (
+          <h1 className="text-2xl font-sembold animate-pulse text-red-500">
+            No result found for "{search}"
+          </h1>
+        )}
       </div>
     </>
   );
